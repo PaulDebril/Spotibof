1d5f85f9d22d00d3ed08ca695917fc1ea862f4af
woorz <marinelangrez@outlook.fr>
2024-02-01 16:37:19 +0100

    feat(core) : update artists list

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import ArtistsList from "../components/artists/artists-list";
+import { ScrollArea } from "../components/ui/scroll-area";
+import { madeForYouAlbums } from "./data/albums";
+
 export default function Home() {
   return (
     <>
+      <ArtistsList />
     </>
   );
 }
