74120dc6e58d10a333c86c8e28ff9ca695d560ec
Clément Berdin <clement.berdin@gmail.com>
2024-02-01 16:30:59 +0100

    feat(service): add getTopAlbums

--- lib/services/index.ts
+++ lib/services/index.ts
@@ -1,3 +1,5 @@
+import {artist} from "@/types";
+
 export async function getTopArtists(page: number = 1,) {
 
     const res = await fetch(
@@ -20,4 +22,28 @@ export async function getTopArtists(page: number = 1,) {
     }
 
     return res.artists.artist;
+}
+
+export async function getTopAlbums( mbid: string, page: number = 1,) {
+
+        const res = await fetch(
+            `http://ws.audioscrobbler.com/2.0/?format=json&method=artist.getTopAlbums&mbid=${mbid}&api_key=62ba5f4a31fc47fdba6cc0639148fbab&page=${page}`,
+            {
+                method: 'GET',
+                redirect: 'follow',
+                next: {
+                    revalidate: 60
+                }
+            })
+            .then(response => response.json())
+            .catch(error => console.log('error', error));
+
+        if (res.error) {
+            return {
+                message: res.message,
+                status: 'error'
+            }
+        }
+
+        return res.topalbums.album;
 }
