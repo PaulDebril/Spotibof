ea96a5f1c2dd4205f2b976f577c8ba616f305fc8
Clément Berdin <clement.berdin@gmail.com>
2024-02-22 14:22:09 +0100

    feat(services): add getArtistTags

--- lib/services/index.ts
+++ lib/services/index.ts
@@ -1,9 +1,9 @@
-import {artist} from "@/types";
+import {artist, tag} from "@/types";
 
 export async function getTopArtists(page: number = 1,) {
 
     const res = await fetch(
-        `http://ws.audioscrobbler.com/2.0/?format=json&method=chart.getTopArtists&api_key=62ba5f4a31fc47fdba6cc0639148fbab&page=${page}`,
+        `http://ws.audioscrobbler.com/2.0/?format=json&method=chart.getTopArtists&api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=${page}`,
         {
             method: 'GET',
             redirect: 'follow',
@@ -21,13 +21,17 @@ export async function getTopArtists(page: number = 1,) {
         }
     }
 
+    res.artists.artist.map(async (artist: artist) => {
+      artist.tags = await getArtistTags(artist.name);
+    });
+
     return res.artists.artist;
 }
 
 export async function getTopAlbums( mbid: string, page: number = 1,) {
 
         const res = await fetch(
-            `http://ws.audioscrobbler.com/2.0/?format=json&method=artist.getTopAlbums&mbid=${mbid}&api_key=62ba5f4a31fc47fdba6cc0639148fbab&page=${page}`,
+            `http://ws.audioscrobbler.com/2.0/?format=json&method=artist.getTopAlbums&mbid=${mbid}&api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=${page}`,
             {
                 method: 'GET',
                 redirect: 'follow',
@@ -46,4 +50,30 @@ export async function getTopAlbums( mbid: string, page: number = 1,) {
         }
 
         return res.topalbums.album;
+}
+
+export async function getArtistTags( artist: string) {
+
+    console.log(encodeURI(artist))
+
+    const res = await fetch(
+        `http://ws.audioscrobbler.com/2.0/?format=json&method=artist.getTopTags&artist=${encodeURI(artist)}&api_key=${process.env.NEXT_PUBLIC_API_KEY}`,
+        {
+            method: 'GET',
+            redirect: 'follow',
+            next: {
+                revalidate: 60
+            }
+        })
+        .then(response => response.json())
+        .catch(error => console.log('error', error));
+
+    if (res.error) {
+        return {
+            message: res.message,
+            status: 'error'
+        }
+    }
+
+  return res.toptags.tag.filter((tag: any) => tag.count == 100);
 }
