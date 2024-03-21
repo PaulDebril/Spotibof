53738275d8450c2afb76cd3381a25bab5d28097e
Clément Berdin <clement.berdin@gmail.com>
2024-03-21 15:17:18 +0100

    fix(ui): update ui

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-black  w-100 ">
+        <footer className="bg-black p-4 text-center mt-6">
             <h1 className='text-green-500 font-bold'>CONTACT</h1>
         </footer>
     );
