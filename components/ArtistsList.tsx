dfe8776d6c6bcdb119549789c5010c474bbf56f0
woorzz <marinelangrez@outlook.fr>
2024-03-21 14:34:16 +0100

    fix(bug-images) : add artists images

--- components/ArtistsList.tsx
+++ components/ArtistsList.tsx
@@ -1,42 +1,60 @@
-import {getTopArtists} from "@/lib/services";
-import {artist} from "@/types";
+import { getImg, getTopAlbums, getTopArtists } from "@/lib/services";
+import { artist } from "@/types";
 import Link from "next/link";
-import {ScrollArea} from "@/components/ui/scroll-area";
-import {madeForYouAlbums} from "@/app/data/albums";
-import {ArtistsCards} from "@/components/artists/artists-cards";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { madeForYouAlbums } from "@/app/data/albums";
+import { ArtistsCards } from "@/components/artists/artists-cards";
 
 export default async function ArtistsList() {
-    const data = await getTopArtists()
+  const data = await getTopArtists();
 
-    if (!data) {
-        return <main>
-            <h1>Top Artists</h1>
-            <p>Loading...</p>
-        </main>
-    }
+  if (!data) {
+    return (
+      <main>
+        <h1>Top Artists</h1>
+        <p>Loading...</p>
+      </main>
+    );
+  }
 
-    if (data.status == "error") {
-        return <main>
-            <h1>Top Artists</h1>
-            <p>Error: {data.message}</p>
-        </main>
-    }
+  if (data.status === "error") {
+    return (
+      <main>
+        <h1>Top Artists</h1>
+        <p>Error: {data.message}</p>
+      </main>
+    );
+  }
 
-    return <main className={''}>
-        <ul className={"flex flex-wrap gap-2"}>
-            {data.map((artist: artist) => (
-              <li key={artist.mbid}>
-                  <Link href={artist.mbid}>
-					<ArtistsCards
-					  artist={artist}
-					  className="w-[150px]"
-					  aspectRatio="square"
-					  width={150}
-					  height={150}
-					/>
-				  </Link>
-              </li>
-            ))}
-        </ul>
+  return (
+    <main className="">
+      <ul
+        className="flex flex-wrap gap-2"
+        style={{
+          display: "flex",
+          alignContent: "center",
+          justifyContent: "center",
+          paddingTop: "50px",
+        }}
+      >
+        {data.map(async (artist: artist) => {
+          const albums = await getImg(artist.mbid);
+          return (
+            <li key={artist.mbid}>
+              <Link href={artist.mbid}>
+                <ArtistsCards
+                  artist={artist}
+                  img={albums}
+                  className="w-[150px]"
+                  aspectRatio="square"
+                  width={150}
+                  height={150}
+                />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </main>
+  );
 }
--- types/index.ts
+++ types/index.ts
@@ -1,35 +1,35 @@
 export type artist = {
-    name: string;
-    playcount: string;
-    listeners: string;
-    mbid: string;
-    url: string;
-    streamable: string;
-    image: image[];
-    tags: tag[];
-}
+  name: string;
+  playcount: string;
+  listeners: string;
+  mbid: string;
+  url: string;
+  streamable: string;
+  image: image[];
+  tags: tag[];
+};
 
 export type image = {
-    '#text': string;
-    size: string;
-}
+  text: string;
+  size: string;
+};
 
 export type album = {
-    name: string;
-    playcount: string;
-    mbid: string;
-    url: string;
-    artist: artist;
-    image: image[];
-}
+  name: string;
+  playcount: string;
+  mbid: string;
+  url: string;
+  artist: artist;
+  image: image[];
+};
 
 export type tag = {
-    count: number;
-    name: string;
-    url: string;
-}
+  count: number;
+  name: string;
+  url: string;
+};
 
 export type error = {
-    message: string;
-    status: string;
-}
+  message: string;
+  status: string;
+};
